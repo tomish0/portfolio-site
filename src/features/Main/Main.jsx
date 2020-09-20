18c087baca5e966704f6031cfa42c733006cefcf
Tom Isherwood <tomish0@outlook.com>
2020-09-20 19:53:47 +0100

    button on dots working

--- src/features/Main/Main.jsx
+++ src/features/Main/Main.jsx
@@ -1,34 +1,42 @@
 import React, { useState } from "react";
-import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
 import Project from "./Project/Project";
 import projects from "../../projects/projects.json";
 import "../../css/Main.css";
-import "pure-react-carousel/dist/react-carousel.es.css";
 
 function Main() {
+  const [position, setPosition] = useState(0);
+  const slide = (index) => {
+    var newPosition = index * -100
+    setPosition(newPosition)
+  };
+
   return (
-    <div>
-      <CarouselProvider
-        naturalSlideWidth={100}
-        naturalSlideHeight={125}
-        totalSlides={projects.length}
-        orientation={'vertical'}
-      >
-        <Slider>
-          {projects.map((project, index) => {
-            return (
-              <Slide
-                className="each-project"
-                index={index}
-                key={index}
-                style={{ backgroundColor: `${project.backgroundColor}` }}
-              >
-                <Project project={project} />
-              </Slide>
-            );
-          })}
-        </Slider>
-      </CarouselProvider>
+    <div className="carousel">
+      <div className="slider">
+        {projects.map((project, index) => {
+          return (
+            <div
+              className="slide"
+              key={index}
+              style={{
+                transform: `translateY(${position}%)`,
+                backgroundColor: `${project.backgroundColor}`,
+              }}
+            >
+              <Project project={project} />
+            </div>
+          );
+        })}
+      </div>
+      <div className="dots">
+        {projects.map((project, index) => {
+          return (
+            <div className="dot" key={index} onClick={() => slide(index)}>
+              <span></span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
