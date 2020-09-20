8ee5258203dc7c6ea9bec6af1dffdd594bc30dfc
Tom Isherwood <tomish0@outlook.com>
2020-09-20 18:56:25 +0100

    working carousel with package but can't scroll

--- src/features/Main/Main.jsx
+++ src/features/Main/Main.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
-import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  DotGroup,
+  Dot,
+} from "pure-react-carousel";
 import Project from "./Project/Project";
 import projects from "../../projects/projects.json";
 import "../../css/Main.css";
 import "pure-react-carousel/dist/react-carousel.es.css";
 
 function Main() {
+
+
   return (
     <div>
       <CarouselProvider
         naturalSlideWidth={100}
-        naturalSlideHeight={125}
+        naturalSlideHeight={100}
         totalSlides={projects.length}
-        orientation={'vertical'}
+        orientation={"vertical"}
+        className={"carousel-provider"}
+        dragEnabled={true}
       >
-        <Slider>
+        <Slider className={"slider"} >
           {projects.map((project, index) => {
             return (
               <Slide
@@ -28,6 +38,7 @@ function Main() {
             );
           })}
         </Slider>
+        <DotGroup className={"dots"} />
       </CarouselProvider>
     </div>
   );
