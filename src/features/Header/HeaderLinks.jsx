b5c802288590e90ee7343766e8c3345985e839f5
Tom Isherwood <tomish0@outlook.com>
2020-09-26 12:53:09 +0100

    rewording

--- src/features/Header/HeaderLinks.jsx
+++ src/features/Header/HeaderLinks.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import '../../css/HeaderLinks.css'
+import "../../css/HeaderLinks.css";
 
 function HeaderLinks(props) {
   return (
-    <div className='header-links-container'>
+    <div className="header-links-container">
       <a href="#">CV</a>
-      <a href="#">Linkedin</a>
-      <a href="#">Github</a>
-      <a href="#">tom.isherwood0@gmail.com</a>
-      <p>+44 7766 311452</p>
+      <a
+        href="https://www.linkedin.com/in/tom-isherwood-a472b6126/"
+        target="_blank"
+      >
+        Linkedin
+      </a>
+      <a href="https://github.com/tomish0" target="_blank">
+        Github
+      </a>
+      <div>tom.isherwood0@gmail.com</div>
+      <div>+44 7766 311452</div>
     </div>
   );
 }
--- src/features/Main/Project/ProjectInfo.jsx
+++ src/features/Main/Project/ProjectInfo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import pdf from "../../../RBuddie-QR-pdf/RBuddie-Qr-Codes-26-09-20.pdf";
 
 function ProjectInfo(props) {
   const { project } = props;
-
   return (
     <div className="project-info-container">
       <p className="project-type">{project.type}</p>
@@ -11,28 +11,36 @@ function ProjectInfo(props) {
       </a>
       <div className="project-description">{project.description}</div>
       <table className="project-tech-table">
-      <tbody>
-        <tr>
-          {project.tech.backEnd.length > 0 ? <th>Front </th> : null}
-          {project.tech.frontEnd.map((item, index) => (
-            <td key={index} className="project-tech-item">
-              {item}
-            </td>
-          ))}
-        </tr>
-        {project.tech.backEnd.length > 0 ? (
+        <tbody>
           <tr>
-            <th>Back </th>
-            {project.tech.backEnd.map((item, index) => (
+            {project.tech.backEnd.length > 0 ? <th>Front </th> : null}
+            {project.tech.frontEnd.map((item, index) => (
               <td key={index} className="project-tech-item">
                 {item}
               </td>
             ))}
           </tr>
-        ) : null}
+          {project.tech.backEnd.length > 0 ? (
+            <tr>
+              <th>Back </th>
+              {project.tech.backEnd.map((item, index) => (
+                <td key={index} className="project-tech-item">
+                  {item}
+                </td>
+              ))}
+            </tr>
+          ) : null}
         </tbody>
       </table>
-      <div>NOTE: {project.note}</div>
+      {project.note ? <div>NOTE: {project.note}</div> : null}
+      {project.title === "RBuddie LTD - Receipt Storage App" ? (
+        <div className="pdf-download-container">
+          <a href={pdf} target="_blank" className="pdf-download">
+            PDF
+          </a>
+          <div>{project.assetNote}</div>
+        </div>
+      ) : null}
     </div>
   );
 }
