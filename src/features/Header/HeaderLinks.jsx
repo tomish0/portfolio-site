f85dae57638cf67af3eceebfe9d11da120ebc9ec
tomish0 <tomish0@outlook.com>
2022-01-03 12:28:39 +0000

    removed hover on links effect

--- src/features/Header/HeaderLinks.jsx
+++ src/features/Header/HeaderLinks.jsx
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faLinkedin, faGithubSquare} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelopeSquare} from '@fortawesome/free-solid-svg-icons';
 
-import "../../css/HeaderLinks.css";
+import '../../css/HeaderLinks.css';
 
-function HeaderLinks(props) {
-  const [hover, setHover] = useState("");
-
-  const hoverEffect = (num) => {
-    setHover(num);
-  };
-
-  const cancelHoverEffect = () => {
-    setHover("");
-  };
-
+function HeaderLinks() {
   return (
     <div className="header-links-container">
       <a
         href="https://www.linkedin.com/in/tom-isherwood-a472b6126/"
         target="_blank"
         rel="noopener noreferrer"
-        onMouseLeave={cancelHoverEffect}
-        onMouseOver={() => hoverEffect(1)}
-        // style={{ color: `${props.color}` }}
-        className={hover === 1 ? "active" : null}
       >
         <FontAwesomeIcon icon={faLinkedin} />
       </a>
@@ -33,19 +19,11 @@ function HeaderLinks(props) {
         href="https://github.com/tomish0"
         target="_blank"
         rel="noopener noreferrer"
-        onMouseLeave={cancelHoverEffect}
-        onMouseOver={() => hoverEffect(2)}
-        // style={{ color: `${props.color}` }}
-        className={hover === 2 ? "active" : null}
       >
         <FontAwesomeIcon icon={faGithubSquare} />
       </a>
       <a
         href="mailto:tom.isherwood0@gmail.com"
-        onMouseLeave={cancelHoverEffect}
-        onMouseOver={() => hoverEffect(3)}
-        // style={{ color: `${props.color}` }}
-        className={hover === 3 ? "active" : null}
       >
         <FontAwesomeIcon icon={faEnvelopeSquare} />
       </a>
